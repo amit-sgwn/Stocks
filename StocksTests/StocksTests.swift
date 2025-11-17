9f1d90b173942ec9f5fa929c7fa7190baf0f58f3
Amit Kumar <amitsangwan898@gmail.com>
2025-11-17 11:46:53 +0530

    Initial Commit

---
+++ StocksTests/StocksTests.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTests.swift
+//  StocksTests
+//
+//  Created by Amit Kumar on 17/11/25.
+//
+
+import Testing
+@testable import Stocks
+
+struct StocksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
